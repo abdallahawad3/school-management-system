37987caced5784837f714ae095c9d40a99d3090b
Abdallahawad3 <djdgdnnxcbj@gmail.com>
2026-01-31 11:03:12 +0200

    add layout suppression warning and enhance menu component with dynamic rendering

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -114,7 +117,32 @@ const menuItems = [
 ];
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <aside className="mt-4 text-sm overflow-y-auto h-[90vh]">
+      {menuItems.map((item) => {
+        return (
+          <section key={item.title}>
+            <h2 className="hidden lg:block text-gray-400 my-4 font-thin">{item.title}</h2>
+            <ul>
+              {item.items.map((subItem) => {
+                return (
+                  <li key={subItem.label}>
+                    <Link
+                      className="flex items-center lg:justify-start lg:gap-1 text-gray-500 py-2"
+                      href={subItem.href}
+                    >
+                      <Image width={24} height={24} src={subItem.icon} alt={subItem.label} />
+                      <span className="hidden lg:block">{subItem.label}</span>
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        );
+      })}
+    </aside>
+  );
 };
 
 export default Menu;
