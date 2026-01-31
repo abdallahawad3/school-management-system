1af21323cb4b2019723d7c38692e69ca09833c19
Abdallahawad3 <djdgdnnxcbj@gmail.com>
2026-01-31 11:25:23 +0200

    add Navbar component and update layout structure to include dynamic rendering

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,16 +11,18 @@ export default function RootLayout({
   return (
     <main className="h-screen flex">
       {/* Left sidebar */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
         <Link href={"/"} className="flex lg:justify-start items-center gap-1">
-          <Image src={"/logo.png"} alt="Logo" width={32} height={32} className="" />
+          <Image priority={true} src={"/logo.png"} alt="Logo" width={32} height={32} className="" />
           <span className="hidden lg:block font-semibold">SchoolMgmt</span>
         </Link>
         <Menu />
       </div>
-      {/* {children} */}
       {/* Right sidebar */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">R</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-auto">
+        <Navbar />
+        {children}
+      </div>
     </main>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <header className="flex justify-between items-center p-4">
+      {/* Search bar */}
+
+      <div className="hidden lg:flex items-center gap-4 border border-gray-300 focus-within:border-blue-500 px-4 py-2 rounded-full shadow-sm w-[300px]">
+        <Image src="/search.png" alt="Search" width={14} height={14} />
+        <input type="text" placeholder="Search..." className="outline-none w-full bg-transparent" />
+      </div>
+
+      {/* Icons */}
+      <div className="flex w-full justify-end items-center gap-4">
+        <div className="bg-white rounded-full p-2 shadow-sm inline-block cursor-pointer">
+          <Image src="/message.png" alt="Notifications" width={20} height={20} />
+        </div>
+        <div className="relative bg-white rounded-full p-2 shadow-sm inline-block cursor-pointer">
+          <Image src="/announcement.png" alt="Announcements" width={20} height={20} />
+          <div className="absolute -top-2 -right-2 size-5 text-xs flex items-center justify-center bg-purple-500 text-white rounded-full border-2 border-white">
+            1
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2 cursor-pointer">
+          <div className="flex flex-col">
+            <h3 className="text-gray-600">John Doe</h3>
+            <p className="text-gray-400 text-sm">Admin</p>
+          </div>
+          <div className="rounded-full p-[2px] border-2 border-gray-500 shadow-sm inline-block">
+            <Image
+              src="/avatar.png"
+              alt="Dropdown"
+              className="rounded-full"
+              width={30}
+              height={30}
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
