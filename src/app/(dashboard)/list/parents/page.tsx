060db65f95f7513a263dfbe30deb3cd12fffd467
Abdallahawad3 <djdgdnnxcbj@gmail.com>
2026-02-03 11:31:07 +0200

    Add parents list page with table, search, and pagination components

---
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -0,0 +1,105 @@
+import Pagination from "@/components/shared/Pagination";
+import Table from "@/components/shared/Table";
+import TableSearch from "@/components/shared/TableSearch";
+import { parentsData, role, studentsData } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+type RowType = {
+  id: number;
+  name: string;
+  email?: string;
+  students: string[];
+  phone: string;
+  address: string;
+  parentId: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Student Names",
+    accessor: "students",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "action",
+  },
+];
+
+const page = () => {
+  const renderRow = (item: RowType) => {
+    return (
+      <tr className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-[#F1F0FF]">
+        <td className="p-4">
+          <div className="flex items-center gap-4">
+            <div>
+              <p className="font-medium">{item.name}</p>
+              <p className="text-sm text-gray-500">{item.email}</p>
+            </div>
+          </div>
+        </td>
+        <td className="hidden md:table-cell">{item.students.join(", ")}</td>
+        <td className="hidden lg:table-cell">{item.phone}</td>
+        <td className="hidden xl:table-cell">{item.address}</td>
+        <td className="flex items-center  py-4 gap-2">
+          <Link href={`/list/parents/${item.parentId}`}>
+            <button className="ml-4 size-7 bg-[#C3EBFA] flex items-center justify-center rounded-full overflow-hidden">
+              <Image src={"/view.png"} alt="delete teacher" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="ml-4 size-7 bg-[#CFCEFF] flex items-center justify-center rounded-full overflow-hidden">
+              <Image src={"/delete.png"} alt="delete teacher" width={16} height={16} />
+            </button>
+          )}
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <section className="bg-white p-4 rounded-md m-4 mt-0">
+      {/* Top */}
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
+        {/* Search And Buttons */}
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex py-2 items-center gap-4 self-end">
+            {/* Buttons */}
+            <button className="size-8 flex items-center justify-center bg-yellow rounded-full">
+              <Image src={"/filter.png"} alt="filter" width={14} height={14} />
+            </button>
+            <button className="size-8 flex items-center justify-center bg-yellow rounded-full">
+              <Image src={"/sort.png"} alt="sort" width={14} height={14} />
+            </button>
+            <button className="size-8 flex items-center justify-center bg-yellow rounded-full">
+              <Image src={"/plus.png"} alt="plus" width={14} height={14} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* List */}
+      <Table columns={columns} data={parentsData} renderRow={renderRow} />
+      {/* Pagination */}
+      <Pagination />
+    </section>
+  );
+};
+
+export default page;
